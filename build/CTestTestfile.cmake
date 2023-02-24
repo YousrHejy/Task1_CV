ac198587cc51528a161ababa594fe7159b2f3729
jooo71 <youseifessam70@gmail.com>
2023-02-24 23:53:21 +0200

    noise and gray

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/PC/Desktop/computer_vision
+# Build directory: C:/Users/PC/Desktop/computer_vision/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
